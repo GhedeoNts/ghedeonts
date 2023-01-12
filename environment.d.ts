fcca03685cc6d9d4b0b3921af4d78e650e0d0b89
GhedeoNts <contact.ntsiassila@gmail.com>
2023-01-12 04:37:51 +0100

    finalisation de la version beta 1.0.0 du porfolio

--- environment.d.ts
+++ environment.d.ts
@@ -2,5 +2,17 @@ declare namespace NodeJS {
     export interface ProcessEnv {
         readonly EMAIL: string;
         readonly EMAIL_PASSWORD: string;
+
+        readonly ENV_VARIABLE: string
+        readonly NEXT_PUBLIC_ENV_VARIABLE: string
+
+        readonly DEVELOPMENT_ENV_VARIABLE: string
+        readonly NEXT_PUBLIC_DEVELOPMENT_ENV_VARIABLE: string
+
+        readonly ENV_LOCAL_VARIABLE: string
+        readonly NEXT_PUBLIC_ENV_LOCAL_VARIABLE: string
+
+        readonly PRODUCTION_ENV_VARIABLE: string
+        readonly NEXT_PUBLIC_PRODUCTION_ENV_VARIABLE: string
     }
 }
